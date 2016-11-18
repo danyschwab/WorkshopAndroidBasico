4b908260a177b33f32316e998d2eb68e545a8ae4
Daniela Schwab <dschwab@confidencecambio.com.br>
2016-11-18 17:01:05 -0200

    Parte de textView

---
+++ app/src/main/java/br/com/danyswork/workshopandroidbasico/TextViewFragment.java
@@ -0,0 +1,32 @@
+package br.com.danyswork.workshopandroidbasico;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class TextViewFragment extends Fragment {
+
+    public TextViewFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_text_view, container, false);
+        TextView textView = (TextView) view.findViewById(R.id.text_view);
+        return view;
+    }
+}
